209f0e37ab086254ffa5d80fc9dba8f048434c5d
Ismael-Oliveira <oliveiraismael2@gmail.com>
2019-07-05 14:56:03 -0300

    método cadastrar na classe contato

--- assets/php/contato.class.php
+++ assets/php/contato.class.php
@@ -9,6 +9,30 @@
             $this->pdo = $pdo;
         }
 
-        
+        public function cadastrar($email, $nome=""){
+
+            if($email == " "){
+                return false;
+            }
+
+            $sql = 'SELECT email FROM contato WHERE email = ?';
+            $sql = $this->pdo->prepare($sql);
+            $sql->bindParam(1, $email);
+            $sql->execute();
+
+            if($sql->rowCount() > 0){
+                return false;
+            }
+
+            $sql = 'INSERT INTO contato (nome, email) VALUES (:nome, :email)';
+
+            $sql = $this->pdo->prepare($sql);
+            $sql->bindParam(':nome', $nome);
+            $sql->bindParam(':email', $email);
+            $sql->execute();
+
+            return true;
+        }
+
     }
 ?>
--- index.php
+++ index.php
@@ -2,4 +2,12 @@
     require('./assets/php/contato.class.php');
 
     $contato = new Contato();
+
+    // testando cadastro
+    $resp = $contato->cadastrar("nana@gmail.com");
+    if($resp){
+        echo 'cadastro realizado com sucesso.';
+    }else{
+        echo 'Erro ao se cadastrar';
+    }
 ?>
